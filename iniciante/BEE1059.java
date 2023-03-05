b8a533e3c07e99dad8b880527cf5675d4a7cd95e
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-05 10:23:35 -0300

    Números Pares

---
+++ iniciante/BEE1059.java
@@ -0,0 +1,12 @@
+package iniciante;
+
+public class BEE1059 {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
+    }
+}
