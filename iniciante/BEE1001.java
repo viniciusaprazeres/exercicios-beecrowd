3f233e8a60cfb511468f6764701751e9a9337193
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-11 12:03:48 -0300

    Extremamente Básico

---
+++ iniciante/BEE1001.java
@@ -0,0 +1,20 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class BEE1001 {
+
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+
+        int a, b, x;
+
+        a = leia.nextInt();
+        b = leia.nextInt();
+        x = a + b;
+
+        System.out.println("X = " + x);
+
+    }
+}
