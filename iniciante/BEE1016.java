7e370db074cf4a88cd82f73dff10e11e75a96e35
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-26 11:59:00 -0300

    Distância

---
+++ iniciante/BEE1016.java
@@ -0,0 +1,18 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class BEE1016 {
+
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+
+        int distancia;
+
+        distancia = leia.nextInt();
+
+        System.out.println(distancia * 2 + " minutos");
+
+    }
+}
